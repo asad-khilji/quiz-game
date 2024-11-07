652d9c7712bd4535c86b421c10f5cfd85722e14e
asad-khilji <100327772+asad-khilji@users.noreply.github.com>
2024-11-06 21:02:25 -0500

    added

---
+++ README.md
@@ -0,0 +1,38 @@
+## Coding Quiz Challenge
+
+## Purpose
+
+* As a coding boot camp student, I want to take a timed quiz on JavaScript fundamentals that stores high scores so that I can gauge my progress compared to my peers.
+
+## Acceptance Criteria
+
+* Given I am taking a code quiz
+* When I click the start button
+* Then a timer starts and I am presented with a question
+* When I answer a question
+* Then I am presented with another question
+* When I answer a question incorrectly
+* Then time is subtracted from the clock
+* When all questions are answered or the timer reaches 0
+* Then the game is over
+* When the game is over
+* Then I can save my initials and score
+
+## Built With
+
+* HTML
+* CSS
+* Javascript
+* Web API
+
+## Website
+
+* https://asad-khilji.github.io/Coding-Quiz/
+
+## Screenshot
+
+* ![Screen Shot 2022-03-23 at 10.46.29 AM.png](https://github.com/asad-khilji/Coding-Quiz/blob/main/assets/images/Screen%20Shot%202022-03-23%20at%2010.46.29%20AM.png)
+
+## Contribution
+
+* Made with ❤️ By Asad Khilji
---
+++ assets/js/questions.js
@@ -0,0 +1,538 @@
+var questions = [
+  {
+    title: "What does the name Allah mean?",
+    choices: ["The Patient", "The Greatest Name", "The Ever Living", "The Most Loving"],
+    answer: "The Greatest Name"
+  },
+  {
+    title: "What does the name Al Ilah mean?",
+    choices: ["The God", "The Creator", "The Originator", "The Guide"],
+    answer: "The God"
+  },
+  {
+    title: "What does the name Al Hayy mean?",
+    choices: ["The Ever Living", "The Patient", "The Protector", "The Judge"],
+    answer: "The Ever Living"
+  },
+  {
+    title: "What does the name Al Qayyum mean?",
+    choices: ["The Self Sustaining", "The King", "The Witness", "The Compassionate"],
+    answer: "The Self Sustaining"
+  },
+  {
+    title: "What does the name Ar Rabb mean?",
+    choices: ["The Provider", "The Master", "The Giver of Life", "The First"],
+    answer: "The Master"
+  },
+  {
+    title: "What does the name Ar Rahman mean?",
+    choices: ["The Extremely Loving and Caring", "The Patient", "The Judge", "The First"],
+    answer: "The Extremely Loving and Caring"
+  },
+  {
+    title: "What does the name Ar Raheem mean?",
+    choices: ["The Creator", "The Always Loving and Caring", "The Forgiving", "The Strong"],
+    answer: "The Always Loving and Caring"
+  },
+  {
+    title: "What does the name Al Maalik mean?",
+    choices: ["The Self Sustaining", "The Owner", "The Reckoner", "The First"],
+    answer: "The Owner"
+  },
+  {
+    title: "What does the name Al Malik mean?",
+    choices: ["The Patient", "The King", "The Creator", "The Noble"],
+    answer: "The King"
+  },
+  {
+    title: "What does the name Al Quddus mean?",
+    choices: ["The Compassionate", "The Blessed", "The One and Only", "The Loving"],
+    answer: "The Blessed"
+  },
+  {
+    title: "What does the name As Subooh mean?",
+    choices: ["The Patient", "The Compassionate", "The Perfect", "The Judge"],
+    answer: "The Perfect"
+  },
+  {
+    title: "What does the name As Salam mean?",
+    choices: ["The Giver of Peace", "The Patient", "The Pure", "The Protector"],
+    answer: "The Giver of Peace"
+  },
+  {
+    title: "What does the name Al Mumin mean?",
+    choices: ["The Giver of Faith", "The Creator", "The Wise", "The Helper"],
+    answer: "The Giver of Faith"
+  },
+  {
+    title: "What does the name Al Muhaymin mean?",
+    choices: ["The Forgiver", "The All Mighty", "The Protector", "The Dominant"],
+    answer: "The Dominant"
+  },
+  {
+    title: "What does the name Al Aziz mean?",
+    choices: ["The Forbearing", "The Patient", "The All Mighty", "The Creator"],
+    answer: "The All Mighty"
+  },
+  {
+    title: "What does the name Al Jabbar mean?",
+    choices: ["The Compassionate", "The Compeller", "The Guide", "The Provider"],
+    answer: "The Compeller"
+  },
+  {
+    title: "What does the name Al Mutakbir mean?",
+    choices: ["The Creator", "The Pardoner", "The Self Sustaining", "The Greatest"],
+    answer: "The Greatest"
+  },
+  {
+    title: "What does the name Al Khaliq mean?",
+    choices: ["The Creator", "The Sustainer", "The Healer", "The Guide"],
+    answer: "The Creator"
+  },
+  {
+    title: "What does the name Al Khalaq mean?",
+    choices: ["The King", "The Frequent Creator", "The Knower", "The Protector"],
+    answer: "The Frequent Creator"
+  },
+  {
+    title: "What does the name Al Bari mean?",
+    choices: ["The Judge", "The Compassionate", "The Originator", "The Guide"],
+    answer: "The Originator"
+  },
+  {
+    title: "What does the name Al Mussawir mean?",
+    choices: ["The Forgiver", "The Fashioner", "The Creator", "The Helper"],
+    answer: "The Fashioner"
+  },
+  {
+    title: "What does the name Al Wahed mean?",
+    choices: ["The All Seeing", "The Sustainer", "The One and Only", "The Noble"],
+    answer: "The One and Only"
+  },
+  {
+    title: "What does the name Al Ahad mean?",
+    choices: ["The Uniquely One", "The Protector", "The Guide", "The Giver"],
+    answer: "The Uniquely One"
+  },
+  {
+    title: "What does the name As Samad mean?",
+    choices: ["The Compassionate", "The Patient", "The Self Sufficient", "The Sustainer"],
+    answer: "The Self Sufficient"
+  },
+  {
+    title: "What does the name Al Ghafar mean?",
+    choices: ["The Creator", "The Most Forgiving", "The Helper", "The Loving"],
+    answer: "The Most Forgiving"
+  },
+  {
+    title: "What does the name Al Ghafoor mean?",
+    choices: ["The Judge", "The Guide", "The Most Forgiving", "The Sustainer"],
+    answer: "The Most Forgiving"
+  },
+  {
+    title: "What does the name Al Afuw mean?",
+    choices: ["The Patient", "The Pardoner", "The Judge", "The Giver"],
+    answer: "The Pardoner"
+  },
+  {
+    title: "What does the name At Tawwab mean?",
+    choices: ["The Compassionate", "The Frequent Repenter", "The Creator", "The Healer"],
+    answer: "The Frequent Repenter"
+  },
+  {
+    title: "What does the name Al Haleem mean?",
+    choices: ["The Most Generous", "The All Seeing", "The Forbearing", "The Noble"],
+    answer: "The Forbearing"
+  },
+  {
+    title: "What does the name Al Kareem mean?",
+    choices: ["The Noble", "The Guide", "The Protector", "The Provider"],
+    answer: "The Noble"
+  },
+  {
+    title: "What does the name Al Akhram mean?",
+    choices: ["The Patient", "The Most Noble", "The Provider", "The Judge"],
+    answer: "The Most Noble"
+  },
+  {
+    title: "What does the name Al Muti mean?",
+    choices: ["The Giver", "The Compassionate", "The All Seeing", "The Judge"],
+    answer: "The Giver"
+  },
+  {
+    title: "What does the name Al Mani mean?",
+    choices: ["The Withholder", "The Guide", "The Helper", "The Protector"],
+    answer: "The Withholder"
+  },
+  {
+    title: "What does the name At Tayyib mean?",
+    choices: ["The Pure", "The Noble", "The Provider", "The Sustainer"],
+    answer: "The Pure"
+  },
+  {
+    title: "What does the name Al Rauf mean?",
+    choices: ["The Most Kind", "The Judge", "The Patient", "The Guide"],
+    answer: "The Most Kind"
+  },
+  {
+    title: "What does the name Al Mubeen mean?",
+    choices: ["The Protector", "The Compassionate", "The Clear", "The Giver"],
+    answer: "The Clear"
+  },
+  {
+    title: "What does the name Al Haqq mean?",
+    choices: ["The All Seeing", "The Guide", "The Ultimate Truth", "The Forgiver"],
+    answer: "The Ultimate Truth"
+  },
+  {
+    title: "What does the name Al Barr mean?",
+    choices: ["The Protector", "The Source of All Good", "The Compassionate", "The Loving"],
+    answer: "The Source of All Good"
+  },
+  {
+    title: "What does the name Al Hameed mean?",
+    choices: ["The Praiseworthy", "The Judge", "The Provider", "The Protector"],
+    answer: "The Praiseworthy"
+  },
+  {
+    title: "What does the name Al Majeed mean?",
+    choices: ["The Glorious", "The Patient", "The Guide", "The Creator"],
+    answer: "The Glorious"
+  },
+  {
+    title: "What does the name Al Wahhab mean?",
+    choices: ["The Judge", "The Protector", "The Bestower of Gifts", "The Guide"],
+    answer: "The Bestower of Gifts"
+  },
+  {
+    title: "What does the name Al Latif mean?",
+    choices: ["The Forgiver", "The Healer", "The All Seeing", "The Subtle"],
+    answer: "The Subtle"
+  },
+  {
+    title: "What does the name Al Fatah mean?",
+    choices: ["The Compassionate", "The Opener", "The Patient", "The Noble"],
+    answer: "The Opener"
+  },
+  {
+    title: "What does the name Al Wadud mean?",
+    choices: ["The Most Loving and Caring", "The Patient", "The Judge", "The Giver"],
+    answer: "The Most Loving and Caring"
+  },
+  {
+    title: "What does the name Al Mannan mean?",
+    choices: ["The Guide", "The Provider", "The Giver of Gifts", "The Pure"],
+    answer: "The Giver of Gifts"
+  },
+  {
+    title: "What does the name Al Wasi mean?",
+    choices: ["The Patient", "The Vast", "The Forgiving", "The Protector"],
+    answer: "The Vast"
+  },
+  {
+    title: "What does the name Al Hafeez mean?",
+    choices: ["The Judge", "The Perserver", "The Patient", "The Compassionate"],
+    answer: "The Perserver"
+  },
+  {
+    title: "What does the name Al Hafiz mean?",
+    choices: ["The Patient", "The Guide", "The Protector", "The Noble"],
+    answer: "The Protector"
+  },
+  {
+    title: "What does the name Al Muqaddim mean?",
+    choices: ["The Judge", "The Noble", "The One who puts first", "The Guide"],
+    answer: "The One who puts first"
+  },
+  {
+    title: "What does the name Al Muakhir mean?",
+    choices: ["The Patient", "The Giver", "The Guide", "The One who puts last"],
+    answer: "The One who puts last"
+  },
+  {
+    title: "What does the name Al Muiz mean?",
+    choices: ["The One who honors", "The Noble", "The Compassionate", "The Guide"],
+    answer: "The One who honors"
+  },
+  {
+    title: "What does the name Al Muzill mean?",
+    choices: ["The Compassionate", "The Patient", "The One who humiliates", "The Sustainer"],
+    answer: "The One who humiliates"
+  },
+  {
+    title: "What does the name As Sayyed mean?",
+    choices: ["The Guide", "The Compassionate", "The One who everyone needs", "The Judge"],
+    answer: "The One who everyone needs"
+  },
+  {
+    title: "What does the name Al Muqit mean?",
+    choices: ["The Guide", "The Faciliator", "The Protector", "The Sustainer"],
+    answer: "The Faciliator"
+  },
+  {
+    title: "What does the name As Sami mean?",
+    choices: ["The Compassionate", "The All Hearing", "The All Seeing", "The Judge"],
+    answer: "The All Hearing"
+  },
+  {
+    title: "What does the name Al Baseer mean?",
+    choices: ["The Patient", "The Guide", "The All Seeing", "The Sustainer"],
+    answer: "The All Seeing"
+  },
+  {
+    title: "What does the name Al Aleem mean?",
+    choices: ["The All Knowing", "The Judge", "The Guide", "The Compassionate"],
+    answer: "The All Knowing"
+  },
+  {
+    title: "What does the name Al Khabir mean?",
+    choices: ["The All Aware", "The Protector", "The Judge", "The Noble"],
+    answer: "The All Aware"
+  },
+  {
+    title: "What does the name Ash Shaheed mean?",
+    choices: ["The Witness", "The Patient", "The Protector", "The Judge"],
+    answer: "The Witness"
+  },
+  {
+    title: "What does the name Al Raqib mean?",
+    choices: ["The All Seeing", "The Observer", "The Patient", "The Noble"],
+    answer: "The Observer"
+  },
+  {
+    title: "What does the name Al Muhit mean?",
+    choices: ["The Guide", "The Judge", "The All Encompassing", "The Compassionate"],
+    answer: "The All Encompassing"
+  },
+  {
+    title: "What does the name Al Haseeb mean?",
+    choices: ["The Noble", "The Guide", "The Reckoner", "The Patient"],
+    answer: "The Reckoner"
+  },
+  {
+    title: "What does the name Ad Dayyan mean?",
+    choices: ["The Guide", "The Sustainer", "The Accountant", "The Judge"],
+    answer: "The Accountant"
+  },
+  {
+    title: "What does the name Al Hakam mean?",
+    choices: ["The Judge", "The Compassionate", "The Guide", "The Sustainer"],
+    answer: "The Judge"
+  },
+  {
+    title: "What does the name Al Hakeem mean?",
+    choices: ["The Wise", "The Patient", "The Guide", "The Sustainer"],
+    answer: "The Wise"
+  },
+  {
+    title: "What does the name Al Qadir mean?",
+    choices: ["The Patient", "The Guide", "The All Powerful", "The Sustainer"],
+    answer: "The All Powerful"
+  },
+  {
+    title: "What does the name Al Qadeer mean?",
+    choices: ["The Guide", "The All Powerful", "The Patient", "The Protector"],
+    answer: "The All Powerful"
+  },
+  {
+    title: "What does the name Al Muqtadir mean?",
+    choices: ["The Sustainer", "The Patient", "The All Powerful", "The Guide"],
+    answer: "The All Powerful"
+  },
+  {
+    title: "What does the name Al Jameel mean?",
+    choices: ["The Most Beautiful", "The Patient", "The Guide", "The Protector"],
+    answer: "The Most Beautiful"
+  },
+  {
+    title: "What does the name As Siteer mean?",
+    choices: ["The Guide", "The Compassionate", "The Concealer", "The Sustainer"],
+    answer: "The Concealer"
+  },
+  {
+    title: "What does the name Ash Shakir mean?",
+    choices: ["The Appreciative", "The Patient", "The Protector", "The Guide"],
+    answer: "The Appreciative"
+  },
+  {
+    title: "What does the name Ash Shakoor mean?",
+    choices: ["The Patient", "The Guide", "The Appreciative", "The Sustainer"],
+    answer: "The Appreciative"
+  },
+  {
+    title: "What does the name Al Wakeel mean?",
+    choices: ["The Disposer of Affairs", "The Patient", "The Guide", "The Protector"],
+    answer: "The Disposer of Affairs"
+  },
+  {
+    title: "What does the name Al Qabid mean?",
+    choices: ["The Withholder", "The Guide", "The Patient", "The Sustainer"],
+    answer: "The Withholder"
+  },
+  {
+    title: "What does the name Al Basit mean?",
+    choices: ["The Guide", "The Extender", "The Sustainer", "The Patient"],
+    answer: "The Extender"
+  },
+  {
+    title: "What does the name Al Kabeer mean?",
+    choices: ["The Patient", "The Guide", "The Grand", "The Protector"],
+    answer: "The Grand"
+  },
+  {
+    title: "What does the name Al Adheem mean?",
+    choices: ["The Patient", "The Guide", "The Magnificent", "The Protector"],
+    answer: "The Magnificent"
+  },
+  {
+    title: "What does the name Al Qawiyy mean?",
+    choices: ["The Patient", "The Guide", "The Most Strong", "The Protector"],
+    answer: "The Most Strong"
+  },
+  {
+    title: "What does the name Al Mateen mean?",
+    choices: ["The Patient", "The Firm", "The Guide", "The Protector"],
+    answer: "The Firm"
+  },
+  {
+    title: "What does the name Al Ali mean?",
+    choices: ["The Guide", "The Most High", "The Patient", "The Protector"],
+    answer: "The Most High"
+  },
+  {
+    title: "What does the name Al A'la mean?",
+    choices: ["The Most High", "The Patient", "The Guide", "The Protector"],
+    answer: "The Most High"
+  },
+  {
+    title: "What does the name Al Muta'al mean?",
+    choices: ["The Guide", "The Protector", "The Most High", "The Patient"],
+    answer: "The Most High"
+  },
+  {
+    title: "What does the name Al Awwal mean?",
+    choices: ["The First", "The Patient", "The Guide", "The Protector"],
+    answer: "The First"
+  },
+  {
+    title: "What does the name Al Akhir mean?",
+    choices: ["The Guide", "The Patient", "The Protector", "The Last"],
+    answer: "The Last"
+  },
+  {
+    title: "What does the name Az Zahir mean?",
+    choices: ["The Patient", "The Apparent", "The Guide", "The Protector"],
+    answer: "The Apparent"
+  },
+  {
+    title: "What does the name Al Batin mean?",
+    choices: ["The Patient", "The Guide", "The Close", "The Protector"],
+    answer: "The Close"
+  },
+  {
+    title: "What does the name Al Jawad mean?",
+    choices: ["The Patient", "The Most Generous", "The Protector", "The Guide"],
+    answer: "The Most Generous"
+  },
+  {
+    title: "What does the name Al Rafiq mean?",
+    choices: ["The Patient", "The Guide", "The Protector", "The Most Gentle"],
+    answer: "The Most Gentle"
+  },
+  {
+    title: "What does the name Al Wali mean?",
+    choices: ["The Friend", "The Patient", "The Guide", "The Protector"],
+    answer: "The Friend"
+  },
+  {
+    title: "What does the name Al Mola mean?",
+    choices: ["The Guide", "The Protector", "The Friend", "The Patient"],
+    answer: "The Protector"
+  },
+  {
+    title: "What does the name An Naseer mean?",
+    choices: ["The Patient", "The Protector", "The Helper", "The Guide"],
+    answer: "The Helper"
+  },
+  {
+    title: "What does the name Al Mujeeb mean?",
+    choices: ["The Guide", "The Patient", "The Responsive", "The Protector"],
+    answer: "The Responsive"
+  },
+  {
+    title: "What does the name Al Qareeb mean?",
+    choices: ["The Patient", "The Protector", "The Near", "The Guide"],
+    answer: "The Near"
+  },
+  {
+    title: "What does the name Al Ghaniyy mean?",
+    choices: ["The Rich", "The Patient", "The Guide", "The Protector"],
+    answer: "The Rich"
+  },
+  {
+    title: "What does the name Al Hadi mean?",
+    choices: ["The Guide", "The Patient", "The Protector", "The Sustainer"],
+    answer: "The Guide"
+  },
+  {
+    title: "What does the name Ash Shafie mean?",
+    choices: ["The Patient", "The Protector", "The Cure", "The Guide"],
+    answer: "The Cure"
+  },
+  {
+    title: "What does the name Al Witr mean?",
+    choices: ["The Guide", "The One and Only", "The Patient", "The Protector"],
+    answer: "The One and Only"
+  },
+  {
+    title: "What does the name An Noor mean?",
+    choices: ["The Patient", "The Protector", "The Light", "The Guide"],
+    answer: "The Light"
+  },
+  {
+    title: "What does the name Al Fatir mean?",
+    choices: ["The Originator", "The Patient", "The Guide", "The Protector"],
+    answer: "The Originator"
+  },
+  {
+    title: "What does the name Al Badi mean?",
+    choices: ["The Patient", "The Guide", "The Protector", "The Originator"],
+    answer: "The Originator"
+  },
+  {
+    title: "What does the name Al Malik ul Mulk mean?",
+    choices: ["The Guide", "The Protector", "The King of All Kings", "The Patient"],
+    answer: "The King of All Kings"
+  },
+  {
+    title: "What does the name Dhul Jalili Wal Iqram mean?",
+    choices: ["The Possessor of Majesty and Honor", "The Patient", "The Protector", "The Guide"],
+    answer: "The Possessor of Majesty and Honor"
+  },
+  {
+    title: "What does the name As Saboor mean?",
+    choices: ["The Patient", "The Guide", "The Protector", "The Sustainer"],
+    answer: "The Patient"
+  },
+  {
+    title: "What does the name Ar Razaaq mean?",
+    choices: ["The Guide", "The Provider", "The Protector", "The Patient"],
+    answer: "The Provider"
+  },
+  {
+    title: "What does the name Al Muhyee mean?",
+    choices: ["The Protector", "The Patient", "The Giver of Life", "The Guide"],
+    answer: "The Giver of Life"
+  },
+  {
+    title: "What does the name Al Mumeet mean?",
+    choices: ["The Patient", "The Protector", "The Guide", "The Giver of Death"],
+    answer: "The Giver of Death"
+  },
+  {
+    title: "What does the name Al Qahhar mean?",
+    choices: ["The Patient", "The Protector", "The Guide", "The Overwhelming"],
+    answer: "The Overwhelming"
+  }
+];
+
